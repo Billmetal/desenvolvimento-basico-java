20d4defa4cf2eb7fa8a9c668008b937e78ab86ca
Bill <billmetal9@gmail.com>
2020-06-23 13:18:12 -0300

    Exemplos referente a aula sobre datas - DateFormat e SimpleDateFormat .

---
+++ Digital Innovation Java/src/main/java/Digital/Innovation/Java/datas_dateformat/Exemplo009.java
@@ -0,0 +1,22 @@
+package Digital.Innovation.Java.datas_dateformat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Exemplo de formatação de data com SimpleDateFormat
+ */
+public class Exemplo009 {
+    public static void main(String[] args) {
+
+        Date agora = new Date();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        String dataFormatada = formatter.format(agora);
+
+        System.out.println(dataFormatada);
+        // 14/07/2019
+
+    }
+}
