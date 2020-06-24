04dbac9d4fa43c01a24e534dd5097624874a2da4
Bill <billmetal9@gmail.com>
2020-06-24 19:34:27 -0300

    Exemplos referente a aula sobre Comparators .

---
+++ Digital Innovation Java/src/main/java/Digital/Innovation/Java/comparators/EstudanteOrdemIdadeReversaComparator.java
@@ -0,0 +1,12 @@
+package Digital.Innovation.Java.comparators;
+
+import java.util.Comparator;
+
+public class EstudanteOrdemIdadeReversaComparator implements Comparator<Estudante>{
+
+	@Override
+	public int compare(Estudante o1, Estudante o2) {
+		return o2.getIdade() - o1.getIdade();
+	}
+
+}
